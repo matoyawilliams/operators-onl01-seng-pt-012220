6313988e03c3a80acfe74115e03f9506e6d3ef0e
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-26 01:35:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
 def unsafe?(speed)
- if speed != 40||60
-  puts "true"
- else
-   "false"
+	if speed > 60 || speed < 40
+		return true;
+	else
+		return false;
+	end
 end
 
+
+
 def not_safe?(speed)
-	speed == 40||60 ? "true" : "false"
-end
+	speed > 60 || speed < 40 ? true : false;
 end
